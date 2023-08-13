049f73898ef9a80eba8a4d0dcfee804a71a8910f
Ceren Uludoğan <62713981+cerenuludogan@users.noreply.github.com>
2023-08-13 13:27:16 +0300

    update

--- Loops.playground/Contents.swift
+++ Loops.playground/Contents.swift
@@ -18,3 +18,29 @@ while x < 10{
 }
 print("Döngü biti")
 
+//for loop
+var myArray = ["Merve","Ayse","Ali"]
+for isim in myArray {
+    print(isim)
+}
+
+var numaraDizisi = [10,20,30,40,70]
+numaraDizisi[0] / 5 * 3
+
+for num in numaraDizisi{
+    print(num / 5 * 3)
+}
+
+//If Statements
+
+//AND &&
+// OR ||
+
+var benimYasim = 9
+if benimYasim < 20 {
+    print("çok gençsin")
+    
+}
+else if benimYasim > 20 && benimYasim < 30 {
+    print("20'li yaşlarındasın")
+}
