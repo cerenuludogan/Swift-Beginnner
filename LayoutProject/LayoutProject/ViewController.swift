d8c789ed1f9fa6c1a6075b3cd7f40c1e48c85ea0
Ceren Uludoğan <62713981+cerenuludogan@users.noreply.github.com>
2023-08-23 16:46:26 +0300

    update

--- LayoutProject/LayoutProject/ViewController.swift
+++ LayoutProject/LayoutProject/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
         //Label
         let myLabel = UILabel()
         myLabel.text = "Text Label"
+        myLabel.textColor = UIColor.blue
         myLabel.textAlignment = .center
         myLabel.frame = CGRect(x: width * 0.5 - width * 0.8 / 2, y: height * 0.5 - 30, width: width * 0.8, height: 60)
         view.addSubview(myLabel)
